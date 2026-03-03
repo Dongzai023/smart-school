1bd0e62987825c6d48d24886f6f7522576c42c65
tree_green <741171285@qq.com>
2026-03-03 15:22:27 +0800

    fix: 修复models导入错误

--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -2,7 +2,7 @@ from app.models.user import User
 from app.models.device import Device, DeviceGroup
 from app.models.schedule import Schedule
 from app.models.lock_image import LockScreenImage
-from app.models.log import SystemLog
+from app.models.log import UnlockRequest, OperationLog
 from app.models.time_slot import TimeSlot
 from app.models.checkin_record import CheckinRecord
 from app.models.leave import Leave
@@ -13,7 +13,8 @@ __all__ = [
     "DeviceGroup",
     "Schedule",
     "LockScreenImage",
-    "SystemLog",
+    "UnlockRequest",
+    "OperationLog",
     "TimeSlot",
     "CheckinRecord",
     "Leave",
